5ee7bc78f9b781b3616b39369bfd8ee6193a02d4
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-02-12 16:22:08 +0900

    ユーザーをdbに登録できる

--- todo/pages/api/auth/[...nextauth].ts
+++ todo/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,22 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import { NextApiHandler } from 'next';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GoogleProvider from "next-auth/providers/google";
+import prisma from '../../../lib/prisma';
 
-export default NextAuth({
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-});
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+  page: {
+    signIn: '/signin',
+  },
+};
