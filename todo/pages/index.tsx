ca9b989679af4c9d9c8bdfd1152ea9fb61e1a031
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-02-13 15:17:31 +0900

    topページ

--- todo/pages/index.tsx
+++ todo/pages/index.tsx
@@ -1,9 +1,23 @@
-
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <div>もももも</div>
-    </>
+    <div>
+      <div>
+        <h1>
+          <span>
+            TODO!!!
+          </span>
+        </h1>
+        <div>
+          <p>さっそくTODOを追加!</p>
+        </div>
+        <div>
+          <Link href="/todos">
+              <span>こちらから</span>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
