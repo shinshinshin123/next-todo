97555b02cdc7b84dbddfb7694976a9ecaf81b3c5
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-02-19 17:42:08 +0900

    ひとまず作成機能完

--- pages/todos/create.tsx
+++ pages/todos/create.tsx
@@ -3,17 +3,28 @@ import Router from "next/router";
 import React from "react";
 
 export default function Create() {
-    //後ほどオブジェクトにして一つにする
-    const [ title, setTitle ] = useState('テスト')
-    const [ content, setContent] = useState('テストです。')
+    //後ほどtitle,contentを一つにまとめる
+    // const [ todo, setTodo ] = useState({
+    //     title: "",
+    //     content: ""
+    // })
+    // const onChangeTitle = (e: any) =>
+    //     setTodo({...todo, title: e.target.value});
+    // const onChangeContent = (e: any) =>
+    //     setTodo({...todo, content: e.target.value});
 
-    const createTodo = async () => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const createTodo = async (e: React.SyntheticEvent) => {
+        e.preventDefault()
         try {
             const body = { title, content }
             await fetch('/api/post', {
                 method: 'POST',
                 body: JSON.stringify(body)
             })
+            await Router.push('/todos')
         } catch(error) {
             console.log(error)
         }
@@ -24,16 +35,14 @@ export default function Create() {
             <div>
                 <h1>Todo作成ページ</h1>
             </div>
-            <button onClick={createTodo}>作成</button>
-
             {/* 入力フォーム */}
-            {/* <form>
+            <form>
                 <p>タイトル</p>
-                    <input type='text' placeholder='タイトル' />
+                    <input placeholder='タイトル' onChange={(e) => setTitle(e.target.value)} />
                 <p>内容</p>
-                    <textarea placeholder='内容'/>
-                <p><button onClick={onClickAdd}>追加</button></p>
-            </form> */}
+                    <textarea placeholder='内容' onChange={(e) => setContent(e.target.value)} />
+                <p><button onClick={createTodo}>作成</button></p>
+            </form>
         </>
     )
 }
