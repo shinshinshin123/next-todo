d47c3a387a23edb2ce5b86febf8cec15d6dee123
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-02-23 23:25:27 +0900

    リンクを追加する

--- pages/todos/create.tsx
+++ pages/todos/create.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Router from "next/router";
 import React from "react";
+import Link from "next/link";
 
 export default function Create() {
     //後ほどtitle,contentを一つにまとめる
@@ -34,6 +35,7 @@ export default function Create() {
         <>
             <div>
                 <h1>Todo作成ページ</h1>
+                <Link href='/todos'>戻る</Link>
             </div>
             {/* 入力フォーム */}
             <form>
