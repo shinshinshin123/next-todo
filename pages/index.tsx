b7e1aff3cf62a5bd3c160edcb6d5981c1fd84a42
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-02-26 12:14:51 +0900

    途中

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: sesion } = useSession()
+  // if(session) {
+  //   return (
+  //     <>
+  //       ログイン {}
+  //     </>
+  //   )
+  // }
   return (
+    
     <>
       <h1>Todoアプリ</h1>
       <Link href='/todos'>todo一覧</Link>
